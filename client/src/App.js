3f07cae2aef6fe6597a9693a426daadf4bcdae9a
Dananjaya Sudasinghe <dananjayasudasinghe@gmail.com>
2022-07-08 09:33:49 +0530

    completed

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Project from "./pages/Project";
+import AddProjectModel from "./components/AddProjectModel";
 
 const cache = new InMemoryCache({
   typePolicies: {
